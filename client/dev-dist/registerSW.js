17d8f489c74d5066aa8a41ac585ff0586e3d3ced
MathieuRosiere <rosiere.mathieu@gmail.com>
2024-03-15 12:34:30 +0100

    Updates

--- client/dev-dist/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/GreenEats/dev-sw.js?dev-sw', { scope: '/GreenEats/', type: 'classic' })
--- client/src/catalog/type.ts
+++
@@ -1,8 +0,0 @@
-export type item = {
-    img: string,
-    id: number;
-    name: string;
-    allergens: string;
-    ingredients: string;
-    palmOil: number;
-}
